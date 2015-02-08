2aaa9675c0e2bf85569f8b4400210d5049a8d2b8
Kevin Murphy <kevin-j-m@users.noreply.github.com>
2015-02-07 21:27:44 -0500

    initial

---
+++ src/app.js
@@ -0,0 +1,40 @@
+var UI = require('ui');
+var ajax = require('ajax');
+
+var card = new UI.Card({
+  title: 'Today\'s Focus',
+  /*icon: 'images/menu_icon.png',*/
+  subtitle: 'Fetching your focus...',
+});
+
+card.show();
+
+var device_id = 'another';
+var URL = "http://help-me-help-you.herokuapp.com/goals/1?device_id=" + device_id;
+
+//Request focus from API
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    // Success!
+    console.log("Successfully fetched today's goal!");
+
+    // Extract data
+    if (data) {
+      var statement = data.statement;
+      // Show to user
+      card.title('Today, I am going to');
+      card.subtitle('');
+      card.body(statement);
+    } else {
+      card.subtitle('Set your goal!');
+    }
+  },
+  function(error) {
+    // Failure!
+    console.log('Failed fetching weather data: ' + error);
+  }
+);
